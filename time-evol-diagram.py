cf0c25e09f171c8735b56c967de1dda58898bee4
hmathlee <hmlee2001@yahoo.com>
2020-11-08 11:44:25 -0500

    Changed color scheme.

--- time-evol-diagram.py
+++ time-evol-diagram.py
@@ -1,42 +1,47 @@
+# Time evolution diagram template. Intended to illustrate how supernova blast radius expands
+#   at a rapidly decreasing rate. The supernova will transition between different phases, at
+#   which the radius is proportional to different powers of time.
+
+# Note: Time is NOT TO SCALE. Please see Jupyter Notebook in this repository for accurate
+#   time scale.
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-lightSpeed = 3 * 10**8
-ejectVel = lightSpeed / np.sqrt(50)
-ismDensity = 1.66 * 10**-20
-heatRatio = 1.625
-
-sedovTaylor = ((0.75 / 16) * (lightSpeed**2 / (np.pi * heatRatio * ismDensity))) ** 0.2
-snowPlow = 1.62 * 10 ** 7
-pDrive = 87089.51
-sunMass = 1.989 * 10**30
-
+# Here, we examine the time evolution of a supernova blast radius of a 1.4 solar-mass star.
 m = 1.4
 
+# Create plot.
 fig = plt.figure()
 ax = plt.axes(xlim=(-90000, 90000), ylim=(-90000, 90000))
+ax = plt.gca()
+ax.set_facecolor('#000000')
 line, = ax.plot([], [], lw=2)
 
-
+# Initialize plot (i.e. get the animation running).
 def init():
     line.set_data([], [])
     return line,
 
-
+# Animate plot.
 def update(i):
     t = np.linspace(0, 2 * np.pi, 100)
     if i <= 50:
         r = 20 * i
+        line.set_color("red")
     elif 50 < i <= 5050:
         rInit = 1000
         r = rInit + 10 * (i - 50)
+        line.set_color("green")
     elif 5050 < i <= 15050:
         rInit = 51000
         r = rInit + 3 * (i - 5050)
+        line.set_color("blue")
     else:
         rInit = 81000
         r = rInit + (i - 15050)
+        line.set_color("black")
     x = r * np.cos(t)
     y = r * np.sin(t)
     line.set_data(x, y)
